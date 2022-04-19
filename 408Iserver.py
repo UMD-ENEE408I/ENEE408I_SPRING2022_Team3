53d77869d9acb89bcd75515a5bf7cf35224ace17
mhildesh <96501410+mhildesh@users.noreply.github.com>
2022-04-18 23:01:15 -0400

    Update 408Iserver.py

--- 408Iserver.py
+++ 408Iserver.py
@@ -7,14 +7,14 @@ port = 80             #ESP32 Server Port
 
 sock.connect((host, port))
 
-message = "Hello World"
+message = b'Hello World'  #for steven on python 2 this was a string "hello world" 
 sock.send(message)
 
-data = ""       
+data = []       
 
 while len(data) < len(message):
     data += sock.recv(1)
 
-print(data)
+print(data) #this prints out an array of bytes we would have to decode back. could also just all use python 2 since that seemed to allow strings
 
 sock.close()
