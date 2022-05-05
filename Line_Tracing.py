258599e4f4c6c71e0e895c10f610d48c2cc7a63b
mhildesh <96501410+mhildesh@users.noreply.github.com>
2022-05-05 11:12:59 -0400

    Add files via upload

---
+++ Line_Tracing.py
@@ -0,0 +1,83 @@
+import math
+import matplotlib.pyplot as plt
+import socket
+
+xVal = []
+yVal = []
+phi = 0.0
+cX = 0
+cY = 0
+cX_prev = 0
+cY_prev = 0
+m1 = 0
+m2 = 0
+m1Prev = 0
+m2Prev = 0
+n1 = 0
+theta = 0
+n2 = 0
+
+s = socket.socket()
+
+host =  "192.168.1.201"  # "192.168.2.104" #put IP address
+port = 80
+
+s.connect((host, port))
+i = 0
+while i < 1000:
+    i = i + 1
+    #n1 = (m1-m1Prev)*(11/360);
+    #n2 = (m2-m2Prev)*(11/360);
+    n1 = n1*(11/360)
+    n2 = n2*(11/360)
+    if n1 == n2:
+        cX = cX + n1 * math.cos(theta*math.pi/180)
+        cY = cY + n1 * math.sin(theta*math.pi/180)
+    else:
+        if n1 < n2: #left turn
+            a = -1
+            smlTurn = n1
+            #print("1eft")
+        else:
+            a = 1
+            smlTurn = n2
+            #print("right")
+
+        r = 4*a*(n1 + n2)/(n1 - n2)
+        rX = cX + r * math.cos((phi-a*90) * (math.pi/180))
+        rY = cY + r * math.sin((phi-a*90) * (math.pi/180))
+        if r == 4:
+            r = r + .001
+        theta = 180*smlTurn/(2*math.pi*(r-4))
+        phi = phi - a*theta
+        cX_prev = cX
+        cY_prev = cY
+        cX = math.cos(theta*math.pi/180)*(cX - rX) + math.sin(theta*math.pi/180)*(cY - rY) + rX
+        cY = -math.sin(theta*math.pi/180)*(cX - rX) + math.cos(theta*math.pi/180)*(cY - rY) + rY
+
+
+    # c = [cosd(theta) -sind(theta); sind(theta) cosd(theta)]*(c - rcoord) + rcoord;
+
+
+    m1Prev = m1
+    m2Prev = m2
+    data = []
+    while len(data) < 2:
+        data.append(s.recv(1))
+
+    n1 = int.from_bytes(data[0], "big", signed="True")
+    n2 = int.from_bytes(data[1], "big", signed="True")
+    #print(cX)
+    #print(cY)
+    xVal.append(cX)
+    yVal.append(cY)
+
+
+plt.xlim(-100,100)
+plt.ylim(-100,100)
+
+plt.plot(xVal, yVal, color = 'red', marker = "o")
+plt.show()
+
+
+s.close()
